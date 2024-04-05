b7f2d0c19e62c0bf7ffe2dd6c1c11b7f3cf4425e
DAMO\Emilio <eormeno@gmail.com>
2024-04-05 13:22:18 -0300

    first commit

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La clave fue reseteada.',
+    'sent' => 'Enviamos a su email el enlace para resetear la clave.',
+    'throttled' => 'Por favor espere antes de reintentar.',
+    'token' => 'El token para resetear la clave es incorrecto.',
+    'user' => "No encontramos un usuario con ese email.",
+
+];
